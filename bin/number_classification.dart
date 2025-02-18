66d77eff7280c716b45b0ba8b05a0aabbbe72be4
Loay Sherif <loaysherif40@gmail.com>
2025-02-18 17:17:32 +0200

    final commit

--- bin/number_classification.dart
+++ bin/number_classification.dart
@@ -4,13 +4,14 @@ void main(List<String> arguments) {
   print("Enter a number");
 // Read and convert input to an integer
   int num = int.parse(stdin.readLineSync()!);
-// Check if the number is even or odd// Check if the number is positive, negative, or zero
+// Check if the number is positive, negative, or zero
   if (num > 0) {
     print("$num is positive");
   } else if (num < 0) {
     print("$num is negative");
   } else {
     print("$num is zero");
+    return;
   }
 // Check if the number is even or odd
   if (num % 2 == 0) {
